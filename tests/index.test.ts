2bb4894e5b3b548825a6592d6230b9b68b5e50f7
Jesse Kelly <kellyjesse881@gmail.com>
2023-11-17 02:30:56 +0100

    Refactor tests and remove unused code

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,23 +1,11 @@
-import { Config, Effect } from "effect";
-import { Database, createAdaptor } from "../src/bun";
+/**
+ * @since 1.0.0
+ */
 
-interface Tables {
-	users: { id: number };
-}
+import { describe, expect, it } from "vitest";
 
-const Database = createAdaptor<Tables>();
-
-const db: Effect.Effect<Database<Tables>, never, void> = Effect.gen(function* (
-	_
-) {
-	const { query } = yield* _(Database.tag);
-	query((db) => db.selectFrom("users"));
+describe("test", () => {
+	it("should work", () => {
+		expect(true).toBe(true);
+	});
 });
-
-const layer = Database.makeLayer(
-	Config.succeed({
-		filename: ":memory:"
-	})
-);
-
-db.pipe(Effect.provide(layer));
